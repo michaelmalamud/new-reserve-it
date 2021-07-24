8e40c27d644367204a225e0662673eabb7c50bae
Michael Malamud <michael.malamud@outlook.com>
2021-07-23 19:38:55 -0500

    test change

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -20,7 +20,7 @@ function Menu() {
           to="/"
         >
           <div className="sidebar-brand-text mx-3">
-            <span>Reserve-It</span>
+            <span>reserve-it</span>
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
