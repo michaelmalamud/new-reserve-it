361a1351903e94bca7f6a656e6e39df7f276ad13
Michael Malamud <michael.malamud@outlook.com>
2021-07-23 20:29:22 -0500

    fewfwe

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+require("dotenv").config({ path: path.join(__dirname, "DATABASE_URL", ".env") });
 
 const express = require("express");
 const cors = require("cors");
