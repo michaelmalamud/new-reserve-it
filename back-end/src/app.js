b201bb2270202841cd99788f973b84d58d1a766d
Michael Malamud <michael.malamud@outlook.com>
2021-07-23 20:36:49 -0500

    ddds

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-require("dotenv").config({ path: path.join(__dirname, "DATABASE_URL", ".env") });
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 const cors = require("cors");
